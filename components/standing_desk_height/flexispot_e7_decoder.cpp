784fddcfa5fbd0cd6820a8641f9b3e231e5d4e62
Bryan Hunwardsen <bryan@hunwardsen.com>
2023-07-02 06:43:17 -0700

    fix hex decode digits, lost in git migration

--- components/standing_desk_height/flexispot_e7_decoder.cpp
+++ components/standing_desk_height/flexispot_e7_decoder.cpp
@@ -83,7 +83,7 @@ bool FlexiSpotE7Decoder::put(uint8_t b) {
 }
 
 float FlexiSpotE7Decoder::decode() {  
-  float height = (buf_[0] * 10) + buf_[1] + (buf_[2] / 10.0));
+  float height = ((buf_[0] * 10) + buf_[1] + (buf_[2] / 10.0));
   // ESP_LOGV("FELXISPOT E7 DECODER", height);
   return height;
 }
@@ -92,23 +92,23 @@ float FlexiSpotE7Decoder::DecodeDigit(uint8_t b) {
   switch (b) {
   case 0x3f:
       return 0.0;
-  case 0x3f:
+  case 0x06:
       return 1.0;
-  case 0x3f:
+  case 0x5b:
       return 2.0;
-  case 0x3f:
+  case 0x4f:
       return 3.0;
-  case 0x3f:
+  case 0x66:
       return 4.0;
-  case 0x3f:
+  case 0x6d:
       return 5.0;
-  case 0x3f:
+  case 0x7c:
       return 6.0;
-  case 0x3f:
+  case 0x07:
       return 7.0;
-  case 0x3f:
+  case 0x7f:
       return 8.0;
-  case 0x3f:
+  case 0x6f:
       return 9.0;
   }
 }
